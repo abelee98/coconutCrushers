d76f655755bc1af79d2d292354a3dfda10a81515
Abraham Lee <leeaj@college.harvard.edu>
2020-01-03 16:43:31 -0500

    added assets (#1)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,17 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let gamepadChannel = FlutterMethodChannel(name: "gamepad",
+                                              binaryMessenger: controller.binaryMessenger)
+    gamepadChannel.setMethodCallHandler({
+      (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+      // Note: this method is invoked on the UI thread.
+      // Handle battery messages.
+      if (call.method == "getGamePadName") {
+        try? print("getGamePadName")
+      }
+    })
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -3,9 +3,24 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<audioplayers/AudioplayersPlugin.h>)
 #import <audioplayers/AudioplayersPlugin.h>
+#else
+@import audioplayers;
+#endif
+
+#if __has_include(<path_provider/PathProviderPlugin.h>)
 #import <path_provider/PathProviderPlugin.h>
+#else
+@import path_provider;
+#endif
+
+#if __has_include(<share/SharePlugin.h>)
 #import <share/SharePlugin.h>
+#else
+@import share;
+#endif
 
 @implementation GeneratedPluginRegistrant
 
